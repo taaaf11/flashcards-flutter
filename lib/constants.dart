6da7079a93b8a49b8afbc0f1cdfec3cfcc2cde74
tofi1130 <taafuuu@gmail.com>
2024-04-16 21:20:31 +0500

    change primary colour

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
-const Color kPrimaryColor = Color(0xff166667);
+// const Color kPrimaryColor = Color(0xff166667);
+const Color kPrimaryColor = Color(0xff3eb489);
