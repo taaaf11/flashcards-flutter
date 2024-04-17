4e1abf1d84129f12233912b6e2f93a256e07a80d
tofi1130 <taafuuu@gmail.com>
2024-04-18 00:04:30 +0500

    add more props to `IconTextButton`

--- lib/components/icon_text_button.dart
+++ lib/components/icon_text_button.dart
@@ -1,25 +1,43 @@
 import 'package:flutter/material.dart';
 
 class IconTextButton extends StatelessWidget {
-  final IconData icon;
+  final Widget icon;
   final Widget child;
+  final double? spacing;
+  final EdgeInsetsGeometry? padding;
+  final EdgeInsetsGeometry? margin;
+  final BorderRadius? borderRadius;
+  final MainAxisAlignment alignment;
+  final Color? hoverColor;
+  final Color? splashColor;
   final Function() onPress;
 
   const IconTextButton(
       {super.key,
       required this.icon,
       required this.child,
+      this.spacing,
+      this.padding,
+      this.margin,
+      this.borderRadius,
+      this.alignment = MainAxisAlignment.start,
+      this.hoverColor,
+      this.splashColor,
       required this.onPress});
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      borderRadius: BorderRadius.circular(12),
+      borderRadius: borderRadius,
+      hoverColor: hoverColor,
+      splashColor: splashColor,
       onTap: onPress,
       child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 12),
+        padding: padding,
+        margin: margin,
         child: Row(
-          children: [Icon(icon), child, const Spacer()],
+          mainAxisAlignment: alignment,
+          children: [icon, SizedBox(width: spacing), child],
         ),
       ),
     );
